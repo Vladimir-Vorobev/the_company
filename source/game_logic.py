22a3bd1e3c5fab304626504a9aea283068c9605b
Vladimir <vovav2005@yandex.ru>
2022-02-12 09:14:09 +0300

    Доработки

--- source/game_logic.py
+++ source/game_logic.py
@@ -39,7 +39,9 @@ class game_logic:
 
     def process_user_choice(self, nick, choice_index, time_out_text=''):
         if nick not in self.users:
+            print(1)
             return
+        print(2)
         consequence = self.current_event['consequences'][choice_index]
         self.users[nick]['balance'] += consequence[1]
         sio.emit(
